8839ecc5274a43408fecc3d363dfb951aa15dd2b
Vimal-5 <vimalvijay561@gmail.com>
2026-02-12 19:04:13 +0530

    Add files via upload

--- C basic/internet data usage alert.c
+++ C basic/internet data usage alert.c
@@ -1,29 +1,15 @@
 #include<stdio.h>
 int main() {
-    int type;
-    float used_data;
-    printf("enter a type & used_data \n");
-    scanf("%d %f",&type,&used_data);
-    switch (type) {
-        case 1: {
-            if (used_data>1)
-            {
-                printf("Speed Reduced");
-            }
-            else {
-                printf("Normal speed");
-            }
-            break;
+    int a,b,c,d;
+    scanf("%d",&a);
+    for (int i=1;i<=a;i++) {
+        scanf("%d",&b);
+        if (b>2) {
+            c++;
         }
-        case 2: {
-            if (used_data>2) {
-                printf("Extra charges applied");
-            }
-            else {
-                printf("Normal speed");
-            }
-            break;
-        }
+        d=d+b;
     }
+    printf("Total data: %d\n",d);
+    printf("High usage days: %d",c);
     return 0;
 }
