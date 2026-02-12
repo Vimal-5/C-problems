f435be43ffd43a3956b56a6acecc295e16afc118
Vimal-5 <vimalvijay561@gmail.com>
2026-02-12 14:50:51 +0530

    Add files via upload

---
+++ C basic/problem 19.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    for (int i=1;i<=a;i++) {
+        int d=i;
+        for (int j=0;j<=a-i;j++) {
+            printf("*");
+        }
+        for (int b=1;b<2*i-1;b++) {
+            printf(" ");
+        }
+        for (int c=a;c>=i;c--) {
+            if (d>=i){
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    for (int i=1;i<=a;i++) {
+        int d=i;
+        for (int j=1;j<=i;j++) {
+            printf("*");
+        }
+        for (int k=1;k<=a*2-2*i;k++) {
+            printf(" ");
+        }
+        for (int c=1;c<=i;c++) {
+            if (d<=i){
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
