8e31b5394a88aa3e636768b4ca5a4956623b95a5
Vimal-5 <vimalvijay561@gmail.com>
2026-02-17 09:36:42 +0530

    Add files via upload

---
+++ C basic/second highest salary in a team.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int size,max=0,s_max=0;
+    scanf("%d",&size);
+    for (int i=0;i<size;i++) {
+        int arr[i];
+        scanf("%d",&arr[i]);
+        if (max<arr[i]) {
+            max=arr[i];
+        }
+        if (s_max<arr[i]&&arr[i]<max) {
+            s_max=arr[i];
+        }
+    }
+    printf("%d",s_max);
+    return 0;
+}
