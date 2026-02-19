377eb2c56b0f35ea7721ce80b849164f422ccbe2
Vimal-5 <vimalvijay561@gmail.com>
2026-02-19 22:14:11 +0530

    Add files via upload

---
+++ C basic/replace stock price with future maximum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int price[n];
+    for (int i=0;i<n;i++) {
+        scanf("%d",&price[i]);
+    }
+    int mod_price[n];
+    for (int j=0;j<n-1;j++) {
+        scanf("%d",&mod_price[j]);
+    }
+    int count=0;
+
+}
