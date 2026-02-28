afd659da1eb93e031865472a69c0c5507cc5e1c7
Vimal-5 <vimalvijay561@gmail.com>
2026-02-28 22:47:07 +0530

    Add files via upload

---
+++ C basic/cosonant counter in speech analysis.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[50];
+    fgets(str,sizeof(str),stdin);
+    int i=0,count=-1;
+    while (str[i]!='\0') {
+        if (str[i]!='a'&&str[i]!='e'&&str[i]!='i'&&str[i]!='o'&&str[i]!='u'&&
+            str[i]!='A'&&str[i]!='E'&&str[i]!='I'&&str[i]!='O'&&str[i]!='U'&&str[i]!=' ') {
+            count++;
+            }
+        i++;
+    }
+    printf("%d",count);
+    return 0;
+}
