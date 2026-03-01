fdde13aa9151b5175684e2ef6db593b6ed68def2
Vimal-5 <vimalvijay561@gmail.com>
2026-03-01 23:23:17 +0530

    Add files via upload

---
+++ C basic/chech prime number using funtion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int prime(int n) {
+    int count=0;
+    for (int i=2;i<=n;i++) {
+        if (n%i==0) {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    (prime(n)==1)?printf("Prime"):printf("Not Prime");
+}
