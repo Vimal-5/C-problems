fb149c421816271bc6b594b4e263198e7611cbb8
Vimal-5 <vimalvijay561@gmail.com>
2026-06-30 20:40:58 +0530

    Add files via upload

---
+++ NumberSeries.c/SumOfOddNumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+
+    int size;
+    printf("Enter the loop size: ");
+    scanf("%d",&size);
+
+    int sum = 0;
+    for (int i=1 ; i<=size ; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+    printf("Sum of odd number is: %d",sum);
+    return 0;
+}
