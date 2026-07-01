c3ee626322181fe049b353ad997099afcc01415a
Vimal-5 <vimalvijay561@gmail.com>
2026-07-01 17:44:17 +0530

    Add files via upload

---
+++ NumberSeries.c/SumOfDigits.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    int sum = 0;
+    while (num != 0) {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    printf("Largest num: %d",sum);
+}
