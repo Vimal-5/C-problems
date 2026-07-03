00ba3d5f0dcf8f723db3c110e0bdb65047610126
Vimal-5 <vimalvijay561@gmail.com>
2026-07-03 18:36:36 +0530

    Add files via upload

---
+++ NumberSeries.c/CountEvenDigits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    int count = 0;
+    while (num != 0) {
+        if ((num % 10) % 2 == 0) {
+            count++;
+        }
+        num = num / 10;
+    }
+    printf("%d",count);
+}
